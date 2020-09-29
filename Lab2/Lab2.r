fdc36d07620b932f729f2a6cfbca287f4d472649
shivam-raj-4 <53818754+shivam-raj-4@users.noreply.github.com>
2020-09-29 15:07:40 +0530

    Add files via upload

---
+++ Lab2/Lab2.r
@@ -0,0 +1,9 @@
+bufftail<-c(10,7,8,9,0)
+gardenbee<-c(1,2,3,4,5)
+redtail<-c(11,12,13,45,67)
+carderbee<-c(6,7,8,44,22)
+honeybee<-c(12,45,89,56,30)
+beeframe<-data.frame(bufftail,gardenbee,redtail,carderbee,honeybee)
+beeframe
+names<-c("thistle","vipers","goldemain","yellowalfala","blackberry")
+rownames(beeframe)<-names(beeframe)
