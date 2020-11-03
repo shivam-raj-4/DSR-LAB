8f0ebb927b964b31500a5d546a0aac7a6e13cb65
Shivam Raj <53818754+shivam-raj-4@users.noreply.github.com>
2020-11-03 19:09:07 +0530

    Delete database.R

--- Lab9/database.R
+++
@@ -1,8 +0,0 @@
-install.packages("RMariaDB")
-
-library(RMariaDB)
-
-con <- dbConnect(RMariaDB::MariaDB(), user='person', password='shivamraj', dbname='person', host='localhost')
-
-res <- dbGetQuery(con, "select * from details;")
-res
