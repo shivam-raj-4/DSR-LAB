0f487a767cdea94c5bed40264df626c3ed836eaa
Shivam Raj <53818754+shivam-raj-4@users.noreply.github.com>
2020-11-03 19:08:38 +0530

    Delete gcookbook.r

--- Lab_21/gcookbook.r
+++
@@ -1,5 +0,0 @@
-install.packages("ggplot2")
-library("ggplot2")
-library("gcookbook")
-cabbage_exp
-ggplot(cabbage_exp,aes(x=Date,y=Weight,fill=Cultivar)) + geom_bar(position="dodge",stat="identity") + geom_text(aes(label=Weight),vjust=1.5, position=position_dodge(.9), colour = "white",size=5)
