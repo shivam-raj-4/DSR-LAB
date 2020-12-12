d6fd8fa23f1044bbfee0552c97eb2d5a6078ed9f
Shivam Raj <53818754+shivam-raj-4@users.noreply.github.com>
2020-12-12 23:57:11 +0530

    Add files via upload

---
+++ Lab_16/Lab_16.R
@@ -0,0 +1,17 @@
+count<-c(9,25,15,2,14,25,24,47)
+speed<-c(2,3,5,9,14,24,29,34)
+fw<-data.frame(count,speed)
+fw
+names<-c("Taw","Torridge","ouse","Exe","Lyn","Brook","Ditch","Fal")
+rownames(fw)=names
+fw
+fw.lm=lm(count ~ speed,data=fw)
+summary(fw.lm)
+names(fw.lm)
+fw.lm$coefficients
+newypred<-fitted(fw.lm)
+newypred
+residuals(fw.lm)
+plot(fw$speed,fw$count,col="red")
+abline(coef(fw.lm), lty=1,col="blue")
+plot(fw.lm,which=1)
